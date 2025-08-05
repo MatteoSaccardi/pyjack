54a8205b120971bb8e689764ffc7d0b37f63c411
MatteoSaccardi <m.saccardi@campus.unimib.it>
2025-08-06 01:50:28 +0200

    correct slicing

--- pyjack/pyjack.py
+++ pyjack/pyjack.py
@@ -421,7 +421,7 @@ class observable:
         return self._new(new_jack_samples)
 
     def __getitem__(self,slice):
-        new_jack_samples = self.jack_samples[slice]
+        new_jack_samples = self.jack_samples[:,*slice]
         new_obs = observable(description=self.description+f', slice {slice}', label=self.label)
         new_obs.create_from_jack_samples(new_jack_samples)
         new_obs.primary = False
