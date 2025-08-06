e926521a3a81852d3e803ce0e1af95940c2f7df2
MatteoSaccardi <m.saccardi@campus.unimib.it>
2025-08-06 02:05:27 +0200

    debugging slicing

--- pyjack/pyjack.py
+++ pyjack/pyjack.py
@@ -425,12 +425,12 @@ class observable:
             if not isinstance(key, tuple):
                 key = (key,)
             new_jack_samples = self.jack_samples[(slice(None),)+key]
-            new_obs = observable(description=self.description+f', slice {key}', label=self.label)
+            new_obs = observable(description=self.description, label=self.label)
             new_obs.create_from_jack_samples(new_jack_samples)
         else:
             mean = self.mean[key]
-            cov = self.cov[key]
-            new_obs = observable(description=self.description+f', slice {key}', label=self.label)
+            cov = self.cov[key,key]
+            new_obs = observable(description=self.description, label=self.label)
             new_obs.create_from_cov(mean,cov)
         new_obs.primary = False
         return new_obs
