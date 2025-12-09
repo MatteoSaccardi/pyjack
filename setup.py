bf1dc1bdacdd9c018a9a55ffdbaf63c2d183cfad
MatteoSaccardi <m.saccardi@campus.unimib.it>
2025-12-09 12:50:19 -0700

    version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjack',
-    version='0.1.0',
+    version='1.0.0',
     author='Matteo Saccardi',
     author_email='matteo.saccardi97@gmail.com',
     description='Jackknife-based observable statistics and error propagation',
